b43545d57164dc860a6cb051b26ae49997e6ba08
Theresa Davis <perugia33@gmail.com>
2023-11-14 12:54:31 -0800

    Integrated MongoDB and Mongoose drivers into app

--- server.js
+++ server.js
@@ -1,7 +1,21 @@
+const mongoose = require('mongoose');
+
+
 const dotenv = require('dotenv');
 
 dotenv.config({ path: './config.env' });
 
+const DB = process.env.DATABASE.replace('<PASSWORD>',  process.env.DATABASE_PASSWORD)
+
+mongoose.connect(DB, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useFindAndModify: false
+}).then(con =>{
+    console.log(con.connections);
+    console.log('DB connection successful')
+})
+// To handle deprecation warnings
 const app = require('./app');
 // Reads the config.env file and loads values into the process.env object.
 
