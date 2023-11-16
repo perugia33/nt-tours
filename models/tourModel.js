0ceae007b545d8ad74620eee656e9de6d34ade55
Theresa Davis <perugia33@gmail.com>
2023-11-15 18:57:22 -0800

    Added mongoose schema and model. Refactored for MVC

--- models/tourModel.js
+++ models/tourModel.js
@@ -1,43 +1,63 @@
-// const mongoose = require('mongoose');
+const mongoose = require('mongoose');
 
 
+// Schema
+tourSchema =  mongoose.Schema({
+    name: {
+        type: String,
+        required: [true, 'Enter the tour name'],     
+        unique: true
+    },
+
+    duration: {
+        type:String
+    },
+
+    price:  {
+        type: Number,
+        required: [true, "Enter the tour's price"] 
+    },
+
+    difficulty: {
+        type: String
+    },
+
+    summary: {
+        type: String,
+        required: [true, 'The tour-summary is required ']
+    },
+
+    description: {
+        type: String,
+    },
+
+    ratingsAverage: {
+        type: Number
+    }
+
+});
+
+// Model
+const Tour = mongoose.model('Tour', tourSchema);
+
+const testTour = new  Tour({
+    name: "Happy Camper Test",
+    duration: "15 days & 14 nights",
+    price: 1350,
+    difficulty: "moderate",
+    summary: "Wander the Quintessentially English Cotswolds Trail ",
+    description: "Discover the enchanting beauty of the Cotswolds. Embark on a 5-day walk through the idyllic Cotswolds region in Gloucester, England. Immerse yourself in quintessentially English country inns, explore ancient commons, and meander through honey-colored villages. ",
+    ratingsAverage: 5.0
+});
 
 
-
-// tourSchema =  mongoose.Schema({
-//     name: {
-//         type: String,
-//         required: [true, 'Enter the tour name'],     
-//     },
-
-//     duration: {
-//         type:String
-//     },
-
-//     price:  {
-//         type: Number,
-//         required: [true, "Enter the tour's price"] 
-//     },
-
-//     difficulty: {
-//         type: String
-//     },
-
-//     summary: {
-//         type: String,
-//         required: [true, 'Enter the tour-summary ']
-//     },
-
-//     description: {
-//         type: String,
-//     },
-
-//     ratingsAverage: {
-//         type: Number
-//     }
-
-// });
+testTour.save().then(doc => {
+    console.log(doc);
+}).catch(err => {
+    console.log('Error : ', err)
+})
 
 
+const Tour = mongoose.model('Tour', tourSchema);
 
-// const Tour = mongoose.model('Tour', tourSchema);
+module.exports = Tour;
--- server.js
+++ server.js
@@ -16,67 +16,7 @@ mongoose.connect(DB, {
 
     console.log('DB connection successful');
 });
-// To handle deprecation warnings
 
-
-// Schema
-tourSchema =  mongoose.Schema({
-    name: {
-        type: String,
-        required: [true, 'Enter the tour name'],     
-        unique: true
-    },
-
-    duration: {
-        type:String
-    },
-
-    price:  {
-        type: Number,
-        required: [true, "Enter the tour's price"] 
-    },
-
-    difficulty: {
-        type: String
-    },
-
-    summary: {
-        type: String,
-        required: [true, 'Enter the tour-summary ']
-    },
-
-    description: {
-        type: String,
-    },
-
-    ratingsAverage: {
-        type: Number
-    }
-
-});
-
-// Model
-const Tour = mongoose.model('Tour', tourSchema);
-
-const testTour = new  Tour({
-    name: "Happy Camper Test",
-    duration: "15 days & 14 nights",
-    price: 1350,
-    difficulty: "moderate",
-    summary: "Wander the Quintessentially English Cotswolds Trail ",
-    description: "Discover the enchanting beauty of the Cotswolds. Embark on a 5-day walk through the idyllic Cotswolds region in Gloucester, England. Immerse yourself in quintessentially English country inns, explore ancient commons, and meander through honey-colored villages. ",
-    ratingsAverage: 5.0
-});
-
-
-testTour.save().then(doc => {
-    console.log(doc);
-}).catch(err => {
-    console.log('Error : ', err)
-})
-
-
-// console.log(process.env);
 
 // Start Server
 const port = process.env.PORT || 3000;
